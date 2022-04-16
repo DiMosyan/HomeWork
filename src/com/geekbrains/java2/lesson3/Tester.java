0248d5be40ebd66d5d882493dee40ab7a022ef63
Di <wi.ar.venom@gmail.com>
2022-04-16 17:04:36 +0300

    part1 of lesson3 complete

---
+++ src/com/geekbrains/java2/lesson3/Tester.java
@@ -0,0 +1,9 @@
+package com.geekbrains.java2.lesson3;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        UniqueWords words = new UniqueWords();
+        words.uniqueWords();
+    }
+}
