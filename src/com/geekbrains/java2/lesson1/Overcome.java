c27ee6ac35f196ad2dcfcea4cb8accb0f7cba4fb
Di <wi.ar.venom@gmail.com>
2022-08-24 13:49:23 +0300

    zero kilometer

--- src/com/geekbrains/java2/lesson1/Overcome.java
+++
@@ -1,4 +0,0 @@
-package com.geekbrains.java2.lesson1;
-
-public interface Overcome {
-}
--- src/com/geekbrains/java2/lesson1/Players.java
+++
@@ -1,8 +0,0 @@
-package com.geekbrains.java2.lesson1;
-
-public interface Players {
-
-    boolean isRun(Track track);
-
-    boolean isJump(Wall wall);
-}
